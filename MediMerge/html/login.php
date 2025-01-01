68027f7b36d80502655f59bfb2e63c90a354f47d
Ranjith S <ranjithselvaraj04@gmail.com>
2025-01-01 19:42:43 +0530

    New Updation

--- MediMerge/html/login.php
+++ MediMerge/html/login.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>Sign Up | Log In</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
@@ -63,6 +63,6 @@
             </div>
         </div>
     </div>
-    <script src="style.js"></script>
+    <script src="/css/style.js"></script>
 </body>
 </html>
